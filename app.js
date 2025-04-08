5e1674fd144af698fd48386a1bf0f51ed4408807
Tanush Gupta <tanushgupta.41@gmail.com>
2025-04-08 22:49:00 +0530

    added correct port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import db from "./db/sqlite.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.PORT;
 
 if (!SERVER_PORT) {
   throw new Error("Forgot to initialze some variables");
